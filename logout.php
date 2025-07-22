fd741e7041f2e15872587fd948e75cdfe5072184
Monsieur-Talleyrand <142813235+Monsieur-Talleyrand@users.noreply.github.com>
2025-07-23 01:11:51 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
-session_start(); // Clear all session variables
-session_destroy(); // Destroy the session
-header('Location: login.php'); // Destroy the session
-exit(); // Destroy the session
+session_start();
+session_destroy(); 
+header('Location: login.php'); 
+exit();
 ?>
